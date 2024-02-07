dbd930676271bfd6038ee4156463689996218c5f
xavier skip <xavierskip@gmail.com>
2024-02-07 15:27:12 +0800

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/',
+  //       destination: '/gift/index.html',
+  //     },
+  //   ]
+  // },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/gift/index.html',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
